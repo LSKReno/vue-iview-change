8401a476e293b67567a3370e5aa652846b211147
NEUmmc <46310131+NEUmmc@users.noreply.github.com>
2019-05-20 14:49:57 +0800

    正在修改职业测试
    对职业测试进行修改

--- src/components/professionTest/professionTest.vue
+++ src/components/professionTest/professionTest.vue
@@ -12,23 +12,29 @@
 	</div>
 	<br><br>
 	 	<div class="card-style" id="startDiv">
-			<div class="question">
-				<span>Q {{index+1}}： </span><span>{{allQuestions.questions[index]}}</span>
+			<!-- 问题 -->
+      
+      <div class="question">
+        <p>{{index+1}}/60</p>
+        <p>{{questions[index].title}}</p>
 			</div>
 			<br><br><br>
-			<div class="answer" v-for="(answer,ind) in allQuestions.answers[index]">
+			
+			<!-- 回答 -->
+			<!-- <div class="answer" v-for="(answer,ind) in allQuestions.answers[index]">
 				<i-Button class="font"  type="ghost" @click="uploadAnswer(ind,index)">
 					{{answer}}
 				</i-Button>
-			</div>
+			</div> -->
 		</div>
-
-			<div class="card-style" id="endDiv" hidden="hidden">
-				<h3>{{finalComment.title}}</h3>
-				<p>{{finalComment.comment}}</p>
+			
+			<!-- 结果 -->
+			<!-- <div class="card-style" id="endDiv" hidden="hidden">
+				<h3></h3>
+				<p></p>
 				<Button @click="again">再测一次</Button>
-			</div>
-		
+			</div> -->
+			<!-- 返回按钮 -->
 			<Button class="back-button" type="text" @click="backToButtonHome" >
 				<Icon type="android-arrow-back" size="32" color="#fff"></Icon>
 			</Button>
@@ -40,42 +46,114 @@
 export default {
 	data(){
 		return {
-			finalComment:{
-				title:"",
-				comment:""
-			},
-			allQuestions:{
-				questions:["你走路时是……",
-				"和人说话时，你……",
-				"你何时感觉最好？",
-				"坐著休息时，你的……",
-				"碰到你感到发笑的事时，你的反应是……",
-				"当你去一个派对或社交场合时，你……",
-				"当你非常专心工作时，有人打断你，你会……",
-				"下列颜色中，你最喜欢哪一颜色？",
-				"你经常梦到你在……",
-				"临入睡的前几分钟，你在床上的姿势是……"],
-				answers:[["大步的快走","小步的快走","不快，仰著头 8面对著世界","不快，低著头","很慢"],
-				["手臂交叠的站著","双手紧握著","一只手或两手放在臀部","碰著或推著与你说话的人","玩著你的耳朵、摸著你的下巴、或用手整理头发"],
-				["早晨","下午及傍晚","夜里"],
-				["两膝盖并拢","两腿交叉","两腿伸直","一腿卷在身下"],
-				["一个欣赏的大笑","笑著，但不大声","轻声的咯咯地笑","羞怯的微笑"],
-				["很大声地入场以引起注意","安静地入场，找你认识的人","非常安静地入场，尽量保持不被注意"],
-				["欢迎他","感到非常恼怒","在上两极端之间"],
-				["红或橘色","黑色","黄或浅蓝色","绿色","深蓝或紫色","白色","棕或灰色"],
-				["落下","打架或挣扎","找东西或人","飞或漂浮","你平常不做梦","你的梦都是愉快的"],
-				["仰躺，伸直","俯躺，伸直","侧躺，微卷","头睡在一手臂上","被盖过头"]],
-				scores:[[6,4,7,2,1],[4,2,5,7,6],[2,4,6],[4,6,2,1],[6,4,3,5],[6,4,2],[6,2,4],[6,7,5,4,3,2,1],
-				[4,2,3,5,6,1],[7,6,4,2,1]]
-			},
-			youQuestions:[],
+			myResults:{},
 			index:0,
-			comments:[["内向的悲观者","人们认为你是一个害羞的、神经质的、优柔寡断的，是须人照顾、永远要别人为你做决定、不想与任何事或任何人有关。他们认为你是一个杞人忧天者，一个永远看到不存在的问题的人。有些人认为你令人乏味，只有那些深知你的人知道你不是这样的人。"],
-			["缺乏信心的挑剔者","你的朋友认为你勤勉刻苦、很挑剔。他们认为你是一个谨慎的、十分小心的人，一个缓慢而稳定辛勤工作的人。如果你做任何冲动的事或无准备的事，你会令他们大吃一惊。他们认为你会从各个角度仔细地检查一切之后仍经常决定不做。他们认为对你的这种反应一部分是因为你的小心的天性所引起的。"],
-			["以牙还牙的自我保护者","别人认为你是一个明智、谨慎、注重实效的人。也认为你是一个伶俐、有天赋有才干且谦虚的人。你不会很快、很容易和人成为朋友，但是是一个对朋友非常忠诚的人，同时要求朋友对你也有忠诚的回报。那些真正有机会了解你的人会知道要动摇你对朋友的信任是很难的，但相等的，一旦这信任被破坏，会使你很难熬过。"],
-			["平衡的中道","别人认为你是一个新鲜的、有活力的、有魅力的、好玩的、讲究实际的、而永远有趣的人；一个经常是群众注意力的焦点，但是你是一个足够平衡的人，不至於因此而昏了头。他们也认为你亲切、和蔼、体贴、能谅解人；一个永远会使人高兴起来并会帮助别人的人"],
-			["吸引人的冒险家","别人认为你是一个令人兴奋的、高度活泼的、相当易冲动的个性；你是一个天生的领袖、一个做决定会很快的人，虽然你的决定不总是对的。他们认为你是大胆的和冒险的，会愿意试做任何事至少一次；是一个愿意尝试机会而欣赏冒险的人。因为你散发的刺激，他们喜欢跟你在一起。"],
-			["傲慢的孤独者","别人认为对你必须“小心处理”。在别人的眼中，你是自负的、自我中心的、是个极端有支配欲、统治欲的。别人可能钦佩你，希望能多像你一点，但不会永远相信你，会对与你更深入的来往有所踌躇及犹豫.世界本来就是层层嵌套，周而复始；不以任何的意志而改变"]]
+			questions:[
+    {"cateName": "常规型", "cateKey": "C", "cateId": 1, "title": "对别人借我的和我借别人的东西，我都能记得很清楚。", "score": 1},
+    {"cateName": "常规型", "cateKey": "C", "cateId": 1, "title": "我喜欢经常请示上级。", "score": 1},
+    {"cateName": "常规型", "cateKey": "C", "cateId": 1, "title": "我是一个沉静而不易动感情的人。", "score": 1},
+    {"cateName": "常规型", "cateKey": "C", "cateId": 1, "title": "我喜欢按部就班地完成要做的工作。", "score": 1},
+    {"cateName": "常规型", "cateKey": "C", "cateId": 1, "title": "我总留有充裕的时间去赴约会。", "score": 1},
+    {"cateName": "常规型", "cateKey": "C", "cateId": 1, "title": "我喜欢把一切安排得整整齐齐、井井有条。", "score": 1},
+    {"cateName": "常规型", "cateKey": "C", "cateId": 1, "title": "对于急躁、爱发脾气的人，我仍能以礼相待。", "score": 1},
+    {"cateName": "常规型", "cateKey": "C", "cateId": 1, "title": "每次写信我都一挥而就，不再重复。", "score": 0},
+    {"cateName": "常规型", "cateKey": "C", "cateId": 1, "title": "我办事很少思前想后。", "score": 0},
+    {"cateName": "常规型", "cateKey": "C", "cateId": 1, "title": "我希望能经常换不同的工作来做。", "score": 0},
+    {"cateName": "现实型", "cateKey": "R", "cateId": 2, "title": "在工作中我喜欢独自筹划，不愿受别人干涉。", "score": 1},
+    {"cateName": "现实型", "cateKey": "R", "cateId": 2, "title": "我喜欢在做事情前，对此事情做出细致的安排。", "score": 1},
+    {"cateName": "现实型", "cateKey": "R", "cateId": 2, "title": "我喜欢亲自动手制作一些东西，从中得到乐趣。", "score": 1},
+    {"cateName": "现实型", "cateKey": "R", "cateId": 2, "title": "我喜欢使用榔头一类的工具。", "score": 1},
+    {"cateName": "现实型", "cateKey": "R", "cateId": 2, "title": "如果掌握一门手艺并能以此为生，我会感到非常满意。", "score": 1},
+    {"cateName": "现实型", "cateKey": "R", "cateId": 2, "title": "我讨厌修理自行车、电器一类的工作。", "score": 0},
+    {"cateName": "现实型", "cateKey": "R", "cateId": 2, "title": "我的动手能力很差。", "score": 0},
+    {"cateName": "现实型", "cateKey": "R", "cateId": 2, "title": "我曾渴望当一名汽车司机。", "score": 0},
+    {"cateName": "现实型", "cateKey": "R", "cateId": 2, "title": "我讨厌跟各类机械打交道。", "score": 0},
+    {"cateName": "现实型", "cateKey": "R", "cateId": 2, "title": "我小时候经常把玩具拆开，把里面看个究竟。", "score": 0},
+    {"cateName": "研究型", "cateKey": "I", "cateId": 3, "title": "我经常不停地思考某一问题，直到想出正确的答案。", "score": 1},
+    {"cateName": "研究型", "cateKey": "I", "cateId": 3, "title": "我喜欢抽象思维的工作，不喜欢动手的工作。", "score": 1},
+    {"cateName": "研究型", "cateKey": "I", "cateId": 3, "title": "我喜欢需要运用智力的游戏。", "score": 1},
+    {"cateName": "研究型", "cateKey": "I", "cateId": 3, "title": "当我工作时，我喜欢避免干扰。", "score": 1},
+    {"cateName": "研究型", "cateKey": "I", "cateId": 3, "title": "我的理想是当一名科学家。", "score": 1},
+    {"cateName": "研究型", "cateKey": "I", "cateId": 3, "title": "我喜欢阅读自然科学方面的书籍和杂志。", "score": 1},
+    {"cateName": "研究型", "cateKey": "I", "cateId": 3, "title": "我很难做那种需要持续集中注意力的工作。", "score": 0},
+    {"cateName": "研究型", "cateKey": "I", "cateId": 3, "title": "我讨厌学数学。", "score": 0},
+    {"cateName": "研究型", "cateKey": "I", "cateId": 3, "title": "在实验室里独自做实验会令我寂寞难耐。", "score": 0},
+    {"cateName": "研究型", "cateKey": "I", "cateId": 3, "title": "遇到难解答的问题时，我常常放弃。", "score": 0},
+    {"cateName": "管理型", "cateKey": "E", "cateId": 4, "title": "我曾经渴望有机会参加探险。", "score": 1},
+    {"cateName": "管理型", "cateKey": "E", "cateId": 4, "title": "和不熟悉的人交谈对我来说毫不困难。", "score": 1},
+    {"cateName": "管理型", "cateKey": "E", "cateId": 4, "title": "当我开始做一件事情后，即使碰到再多的困难，我也要执著地干下去。", "score": 1},
+    {"cateName": "管理型", "cateKey": "E", "cateId": 4, "title": "我总是主动地向别人提出自己的建议。", "score": 1},
+    {"cateName": "管理型", "cateKey": "E", "cateId": 4, "title": "我更喜欢自己下了赌注的比赛或游戏。", "score": 1},
+    {"cateName": "管理型", "cateKey": "E", "cateId": 4, "title": "如果待遇相同，我宁愿当商品推销员，而不愿当图书管理员。", "score": 1},
+    {"cateName": "管理型", "cateKey": "E", "cateId": 4, "title": "我喜欢在人事部门工作。", "score": 1},
+    {"cateName": "管理型", "cateKey": "E", "cateId": 4, "title": "在集体讨论中，我往往保持沉默。", "score": 0},
+    {"cateName": "管理型", "cateKey": "E", "cateId": 4, "title": "我愿意从事虽然工资少、但是比较稳定的职业。", "score": 0},
+    {"cateName": "管理型", "cateKey": "E", "cateId": 4, "title": "和别人谈判时，我总是很容易放弃自己的观点", "score": 0},
+    {"cateName": "社会型", "cateKey": "S", "cateId": 5, "title": "我很容易结识同性朋友。", "score": 1},
+    {"cateName": "社会型", "cateKey": "S", "cateId": 5, "title": "我乐于解除别人的痛苦。", "score": 1},
+    {"cateName": "社会型", "cateKey": "S", "cateId": 5, "title": "我喜欢作一名教师。", "score": 1},
+    {"cateName": "社会型", "cateKey": "S", "cateId": 5, "title": "大家公认我是一名勤劳踏实的、愿为大家服务的人", "score": 1},
+    {"cateName": "社会型", "cateKey": "S", "cateId": 5, "title": "我喜欢把一件事情做完后再做另一件事。", "score": 0},
+    {"cateName": "社会型", "cateKey": "S", "cateId": 5, "title": "当我一个独处时，会感到更愉快。", "score": 0},
+    {"cateName": "社会型", "cateKey": "S", "cateId": 5, "title": "我喜欢参加各种各样的聚会。", "score": 0},
+    {"cateName": "社会型", "cateKey": "S", "cateId": 5, "title": "对于社会问题，我通常持中庸的态度。", "score": 0},
+    {"cateName": "社会型", "cateKey": "S", "cateId": 5, "title": "听别人谈“家中被盗”一类的事，很难引起我的同情。", "score": 0},
+    {"cateName": "社会型", "cateKey": "S", "cateId": 5, "title": "和一群人在一起的时候，我总想不出恰当的话来说。", "score": 0},
+    {"cateName": "艺术型", "cateKey": "A", "cateId": 6, "title": "我喜欢做戏剧、音乐、歌舞、新闻采访等方面的工作。", "score": 1},
+    {"cateName": "艺术型", "cateKey": "A", "cateId": 6, "title": "我喜欢成为人们注意的焦点。", "score": 1},
+    {"cateName": "艺术型", "cateKey": "A", "cateId": 6, "title": "我喜欢不时地夸耀一下自己取得的好成就。", "score": 1},
+    {"cateName": "艺术型", "cateKey": "A", "cateId": 6, "title": "音乐能使我陶醉。", "score": 1},
+    {"cateName": "艺术型", "cateKey": "A", "cateId": 6, "title": "有些人太霸道，有时明明知道他们是对的，也要和他们对着干。", "score": 1},
+    {"cateName": "艺术型", "cateKey": "A", "cateId": 6, "title": "我爱幻想。", "score": 1},
+    {"cateName": "艺术型", "cateKey": "A", "cateId": 6, "title": "当接受新任务后，我喜欢以自己的独特方法去完成它。", "score": 1},
+    {"cateName": "艺术型", "cateKey": "A", "cateId": 6, "title": "我有文艺方面的天赋。", "score": 1},
+    {"cateName": "艺术型", "cateKey": "A", "cateId": 6, "title": "看情感影片时，我常禁不住眼圈红润。", "score": 1},
+    {"cateName": "艺术型", "cateKey": "A", "cateId": 6, "title": "与言情小说相比，我更喜欢推理小说。", "score": 0}
+],
+			results:[
+  {
+    cateKey: 'R',
+    cateName: '实际型',
+    score: 0,
+    type: '家里你说了算',
+    info: '愿意使用工具从事操作性工作，动手能力强，做事手脚灵活，动作协调。偏好于具体任务，不善言辞，做事保守，较为谦虚。缺乏社交能力，通常喜欢独立做事。'
+  },
+  {
+    cateKey: 'C',
+    cateName: '常规型',
+    score: 0,
+    type: '非常细心的人，另一半很幸福',
+    info: '尊重权威和规章制度，喜欢按计划办事，细心、有条理，习惯接受他人的指挥和领导，自己不谋求领导职务。喜欢关注实际和细节情况，通常较为谨慎和保守，缺乏创造性，不喜欢冒险和竞争，富有自我牺牲精神。'
+  },
+  {
+    cateKey: 'E',
+    cateName: '企业型',
+    score: 0,
+    type: '你就是传说中的领导',
+    info: '追求权力、权威和物质财富，具有领导才能。喜欢竞争、敢冒风险、有野心、抱负。为人务实，习惯以利益得失，权利、地位、金钱等来衡量做事的价值，做事有较强的目的性。'
+  },
+  {
+    cateKey: 'S',
+    cateName: '社会型',
+    score: 0,
+    type: '没办法，你到处都有朋友',
+    info: '喜欢与人交往、不断结交新的朋友、善言谈、愿意教导别人。关心社会问题、渴望发挥自己的社会作用。寻求广泛的人际关系，比较看重社会义务和社会道德。'
+  },
+  {
+    cateKey: 'A',
+    cateName: '艺术型',
+    score: 0,
+    type: '没错，艺术家就该留长发',
+    info: '有创造力，乐于创造新颖、与众不同的成果，渴望表现自己的个性，实现自身的价值。做事理想化，追求完美，不重实际。具有一定的艺术才能和个性。善于表达、怀旧、心态较为复杂。'
+  },
+  {
+    cateKey: 'I',
+    cateName: '研究型',
+    score: 0,
+    type: '很牛逼（除了头发）',
+    info: '思想家而非实干家,抽象思维能力强，求知欲强，肯动脑，善思考，不愿动手。喜欢独立的和富有创造性的工作。知识渊博，有学识才能，不善于领导他人。考虑问题理性，做事喜欢精确，喜欢逻辑分析和推理，不断探讨未知的领域。'
+  }
+]
 		}
 	},
 	methods: {
@@ -84,58 +162,18 @@ export default {
                  path: '/buttonHome'
             });        
 		},
+
 		startTest(){
 
 		},
+
 		again(){
 			this.index=0;
 			document.getElementById("startDiv").hidden=null;
 			document.getElementById("endDiv").hidden="hidden";
-			this.youQuestions=[];
+			this.myResults = {};
 		},
-		uploadAnswer(index,newIndex){
-			if(newIndex==9){
-				let score=0;
-				this.youQuestions.push(index)
-				console.log(this.youQuestions)
-				for(let i=0;i<this.youQuestions.length;i+=1){
-					score+=this.allQuestions.scores[i][this.youQuestions[i]]
-				}
-				if(score<21){
-					this.finalComment.title=this.comments[0][0];
-					this.finalComment.comment=this.comments[0][1];
-				}
-				else if(score<=30){
-					this.finalComment.title=this.comments[1][0];
-					this.finalComment.comment=this.comments[1][1];
-				}
-				else if(score<=40){
-					this.finalComment.title=this.comments[2][0];
-					this.finalComment.comment=this.comments[2][1];
-				}
-				else if(score<=50){
-					this.finalComment.title=this.comments[3][0];
-					this.finalComment.comment=this.comments[3][1];
-				}
-				else if(score<=60){
-					this.finalComment.title=this.comments[4][0];
-					this.finalComment.comment=this.comments[4][1];
-				}
-				else {
-					this.finalComment.title=this.comments[5][0];
-					this.finalComment.comment=this.comments[5][1];
-					}
-				console.log(this.finalComment.title)
-				console.log(this.finalComment.comment)
-				document.getElementById("startDiv").hidden="hidden";
-				document.getElementById("endDiv").hidden=null;
-
-			}
-			else{
-				this.youQuestions.push(index)
-				this.index+=1;
-			}
-		}
+		
 	}
 }
 </script>
@@ -143,10 +181,8 @@ export default {
 <style>
 .question{
 	font-family: serif;
-	font-size: 2rem;
-	display: flex;
-	align-items: center;
-	justify-content: center;
+	font-size: 1.8rem;
+	color: black;
 }
 .answer{
 	display:flex;
@@ -154,11 +190,11 @@ export default {
 	justify-content:center;
 }
 .card-style{
-	background-color: #dd7fbeef;
+	background-color: #f4f8faef;
 	width: 50rem;
-	height: 22.5rem;
+	height: 25rem;
 	margin: 5rem auto 0;
-	box-shadow: 0.0625rem 0.4375rem 1.5625rem #fd8fd9;
+	box-shadow: 0.0625rem 0.4375rem 1.5625rem #c9c7c8;
 }
 .breadcrumb{
 	padding:0.65rem;
@@ -169,7 +205,7 @@ export default {
 	bottom: 2rem;
 }
 .font{
-	color: aliceblue
+	color: aliceblue;
 }
 #big{
 	color:white;
